bd46cee1179b74a0096d1152623d106e00834ed9
Annie <smartherd@gmail.com>
2016-07-16 17:26:13 +0530

    MyMaterialDesign complete project

---
+++ app/src/main/java/com/smartherdannie/mymaterialdesign/MainActivity.java
@@ -0,0 +1,23 @@
+package com.smartherdannie.mymaterialdesign;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here
+        } else { // For Below API 21
+            // Implement this feature without material design
+        }
+
+    }
+}
